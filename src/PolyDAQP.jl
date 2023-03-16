df57011055f0e1d48e2000ae9b44cb0d1663dd8b
Daniel Arnström <daniel.arnstrom@liu.se>
2023-03-16 18:40:47 +0100

    Update .toml and Module includes/exports.

--- src/PolyDAQP.jl
+++ src/PolyDAQP.jl
@@ -2,6 +2,20 @@ module PolyDAQP
 using DAQP, LinearAlgebra
 
 include("core.jl")
-export center,minrep,slice,iscontained,bounding_box 
+export slice,iscontained
+
+include("center.jl")
+export center
+
+include("minrep.jl")
+export minrep
+include("vrep.jl")
+
+include("boundingbox.jl")
+export bounding_box 
+
+include("types.jl")
+export Polyhedron
+include("plot.jl")
 
 end # module
